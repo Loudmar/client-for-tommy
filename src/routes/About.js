d7a193bdd6457252994d3f1591623fbb79dd8369
Loudmar <loud.tur@gmail.com>
2023-02-24 17:33:34 +0000

    second commit

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <div className='about'>
-      <h1>About</h1>
-    </div>
+    <section className='about about-bg'>
+      <div className='container'>
+        <h1>ABOUT</h1>
+        <div className='container-head'>
+          <img className='author-profile' src='/about-img.jpeg' alt='Author'></img>
+          <h2>Hello - I am Tommy - Welcome to my page.</h2>
+        </div>
+        <div className='container-body'>
+          <div className='container-body-text'>
+            <p>I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself.</p>
+            <p>I have been writing poems since I was teenager. This started as a joke and it has now became a hobby I love.</p>
+            <p>I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself. I am a part-time writer who lover to write poems. Here you will be able to see some of my work and know more about myself.</p>
+          </div>
+          <div className='container-body-img'>
+            <img className='about-img' src='/little.jpg' alt='Little boy'></img>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Projects.css';
 
 function Projects() {
   return (
-    <div className='projects'>
-      <h1>Projects</h1>
-    </div>
+    <section className='projects projects-bg'>
+      <div className='project-container'>
+        <h1>PROJECTS</h1>
+        <h3>Charity works</h3>
+        <div className='project-container-body project-container-body-first'>
+          <div className='project-container-text'>
+            <p>One of the projects I work with and support is the charity of super-heros.</p>
+            <p>We are a group of volunteers that will dress up as a super heros to visit sick children in hospitals.</p>
+            <p>My character is based on the Harry Potter, which all the children loves and makes them smile.</p>
+          </div>
+          <div className='project-container-img'>
+            <img className='author-img' src='/project-img.jpg' alt='Dressed as Harry Potter'></img>
+          </div>
+        </div>
+        <div className='project-container-body'>
+          <div className='project-container-text'>
+            <p>The visits take places on my day offs and over the weekend. We go to the hospitals during visiting hours and ask permission to their parents before we can see them.</p>
+            <p>It is important to adhere to the health and safety of the children. All of them is going through treatment to cancer.</p>
+            <p>The best part of this project is seeing the smile on their faces.</p>
+          </div>
+          <div className='project-container-img'>
+            <img className='author-img1' src='/project-img1.jpeg' alt='Visiting one of the kinds'></img>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
