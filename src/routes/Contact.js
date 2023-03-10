8047f8805413977ddbc38679c4b13fef528e7fa5
Loudmar <loud.tur@gmail.com>
2023-03-10 17:33:38 +0000

    Search bar added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,109 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import {
+  FaUser,
+  FaRegEnvelope,
+  FaRegCommentAlt,
+  FaNewspaper,
+  FaFacebook,
+  FaInstagram
+} from "react-icons/fa";
 import './Contact.css';
 
 function Contact() {
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const subjectRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const dataInput = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      subject: subjectRef.current.value,
+      message: messageRef.current.value
+    }
+    alert("tadaaa!: \n" + JSON.stringify(dataInput) + "Your data 😎")
+  }
+
   return (
     <div className='contact contact-bg'>
-      <h1>Contact</h1>
+      <div className="contact-container">
+      <h2>Contact me</h2>
+      <p>If you would like to get in touch, please e-mail me below, or alternatevely via my social media. Links also below. </p>
+      <div className="contact-social">
+        <a target="_blank" href="https://facebook.com/tommy.bento.7/"><FaFacebook /></a>
+        <a target="_blank" href="https://www.instagram.com/tommybento/"><FaInstagram /></a>
+      </div>
+      <div className="form-container">
+        <form action="" onSubmit={handleSubmit} className="form" id="contact-form">
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-input" 
+              placeholder="Enter your full name" 
+              id="name" 
+              name="name" 
+              minlength="3" 
+              tabIndex="1"
+              ref={nameRef}
+              required 
+            />
+              <FaUser />
+          </div>
+          <div className="form-group">
+            <input 
+              type="email" 
+              className="form-input" 
+              placeholder="Enter your email" 
+              id="email" 
+              name="email" 
+              tabIndex="2"
+              ref={emailRef}
+              required 
+            />
+              <FaRegEnvelope />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-input" 
+              id="subject" 
+              name="subject" 
+              placeholder="Subject" 
+              tabIndex="3"
+              ref={subjectRef}
+              required 
+            />
+              <FaNewspaper />
+          </div>
+          <div className="form-group">
+            <textarea 
+              required 
+              className="form-input form-textarea" 
+              id="message" 
+              name="message"
+              placeholder="How may I help you?" 
+              minlength="15"
+              tabIndex="4"
+              ref={messageRef}
+            >            
+            </textarea>
+            <FaRegCommentAlt />  
+          </div>
+          <div className="form-group buttons">
+            <button type="submit" className="btn btn-primary">
+              SEND
+            </button>
+            <button type="reset" className="btn btn-primary">
+              CANCEL
+            </button>                 
+          </div>
+        </form>
+      </div>
+    </div>
     </div>
   )
 }
--- src/routes/Poems.js
+++ src/routes/Poems.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
+import { FaArrowDown, FaArrowUp, FaSistrix } from 'react-icons/fa';
 import './Poems.css';
 
 function Poems() {
 
   const [data, setData] = useState([]);
   const [selected, setSelected] = useState(null);
+  const [search, setSearch] = useState("");
 
   const toggle = (i) => {
     if (selected === i) {
@@ -24,15 +25,28 @@ function Poems() {
     return <div>Loading...</div>
   }
 
+  const filteredPosts = data.filter(
+    (post) =>
+      post.title.toLowerCase().includes(search.toLowerCase()) ||
+      post.body.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className='poems poems-bg collapse-container'>
       <div className='poem-title-container'>
-        <h2 className='poem-title'>Collection Poems</h2>
+        <h2 className='poem-title'>Poem's Collection</h2>
         <p>Here you will find a collection of my poems. I hope you enjoy reading as much I enjoy writing it.</p>
       </div>
-      
+      <div className="search-container">
+        <FaSistrix />
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Poems"
+        />
+      </div>
       <div className="collapse">
-        {data.map((post, i) => (
+        {filteredPosts.map((post, i) => (
           <div className="collapse-item">
             <div className="collapse-title" onClick={() => toggle(i)}>
               <h3>{post.title}</h3>
